f1de8a9b8e94559dcd9691fdd1013771ece19e13
John D. Ament <johndament@apache.org>
2017-06-16 19:45:17 -0400

    Adding build config.

--- build_site.sh
+++ build_site.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 rm -rf /tmp/incubator-site
-gradle jbake
-mkdir /tmp/incubator-site
-cp -R build/jbake/* /tmp/incubator-site
+jbake -b . /tmp/incubator-site/
 git checkout asf-site
 git clean -f -d
 git pull origin asf-site
